b3af97a4b641a959b6570b112db13cb625521f7a
beelarr <bryonl@me.com>
2017-12-09 19:07:25 -0600

    Add Video Animation

--- components/scenes/layouts/elements/VideoElement.js
+++ components/scenes/layouts/elements/VideoElement.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react';
 import {
     Video,
     View,
-    asset
+    asset,
+    Animated
 } from 'react-vr';
 
+import { Easing } from 'react-native';
+
 export default class VideoElement extends Component {
+
+    state = {
+        fadeIn: new Animated.Value(0)
+    }
+
+    componentDidMount() {
+        Animated.sequence([
+            Animated.timing(
+                this.state.fadeIn,
+                {
+                    toValue: 1,
+                    duration: 3000,
+                    easing: Easing.ease
+                }
+            )
+        ]).start()
+}
     render() {
         return (
-            <View
+            <Animated.View
                 style={{
                     margin: 0.1,
-                    height: 4
+                    height: 4,
+                    opacity: this.state.fadeIn
                 }}>
                 <Video
                     style={{ height: 4 }}
                     source={asset('fireplace.mp4')}
                 />
 
-            </View>
+            </Animated.View>
         )
     }
 }
