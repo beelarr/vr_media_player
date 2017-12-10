10475ed04811de12ac82e0592dbc1d2581cbd539
beelarr <bryonl@me.com>
2017-12-09 19:25:49 -0600

    Twitch API is working

---
+++ components/creds/creds.js
@@ -0,0 +1,3 @@
+const client_id = 'qd8kqvcvmjm0ak2upjwy0k4buxqu0h';
+
+module.exports = client_id;
--- index.vr.js
+++ index.vr.js
@@ -3,11 +3,26 @@ import {
   AppRegistry,
   View,
 } from 'react-vr';
+import axios from 'react-native-axios';
+import client_id from './components/creds/creds';
+
 import TitleScene from './components/scenes/TitleScene';
 import Dashboard from './components/scenes/Dashboard';
 import VideoPlayer from './components/scenes/VideoPlayer';
 
+
 export default class media_player extends React.Component {
+
+  componentWillMount(){
+    axios.get(`https://api.twitch.tv/kraken/streams/featured?limit=6&client_id=${client_id}`)
+        .then(data => {
+          console.log('Data', data);
+        })
+        .catch(e => {
+          console.log('Error', e.message);
+        });
+  }
+
   render() {
     return (
       <View>
